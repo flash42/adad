f24e25fbf0ea95b6915bd0dfd74f006a4c7222cf
Adam Peresztegi <adam.peresztegi@prezi.com>
2014-06-22 16:34:42 +0200

    Implementing gameplay -> low level methods
    - use map for game-state
    - merge-left method
    - keypress events

--- src/adad.cljs
+++ src/adad.cljs
@@ -30,13 +30,48 @@
   (dommy/set-text! (class-sel1 (str row col)) value))
 
 (defn- update! [state]
-  (doseq [[x row](map-indexed vector state)
-          [y value] (map-indexed vector row)]
+  (doseq [[x row] (map identity state)
+          [y value] (map identity row)]
       (update-cell! x y value)))
 
 (defn ^:export gameloop []
   (do
     (create-board 4)
-    (update! game-state))
+    (update! game-state)))
+
+(defn step-state
+  [evt]
+  ; left = 37
+  ; up = 38
+  ; right = 39
+  ; down = 40
+  (when (= 37 (.-keyCode evt))
+    (update! (calc-state! :left game-state))))
+
+(dommy/listen! (sel1 :body) :keyup step-state!)
+
+(defn get-col [col-idx state]
+  (apply hash-map
+         (flatten
+          (map #(list (first %) (get (second %) col-idx))
+               (seq state)))))
+
+(defn get-row [row-idx state]
+  (get state row-idx))
+
+(defn merge-left [row-idx state]
+  (let [row (get state row-idx)]
+    (for [[col-idx val]
+          (map-indexed list
+                       (filter #(not= 0 (second %)) (get-row row-idx state)))]
+      (assoc state row-idx (assoc {0 0, 1 0, 2 0, 3 0} col-idx val)))))
+
+(merge-left 0 game-state)
+
+(def
+  game-state
+  (assoc game-state 0 (assoc (get game-state 0) 0 5)))
+game-state
+
 
 
