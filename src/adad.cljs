7b86459a50d37717ec9cf312d5b474d743c7b9fb
Adam Peresztegi <adam.peresztegi@prezi.com>
2014-07-02 23:37:40 +0200

    Added left-merge

--- src/adad.cljs
+++ src/adad.cljs
@@ -7,7 +7,7 @@
 
 
 (def game-state
-  {0 {0 2, 1 0, 2 0, 3 0},
+  {0 {0 2, 1 0, 2 0, 3 2},
    1 {0 2, 1 0, 2 0, 3 0},
    2 {0 2, 1 0, 2 0, 3 0},
    3 {0 2, 1 0, 2 0, 3 0}})
@@ -22,7 +22,7 @@
   (let [row (node [:tr])]
     (dommy/append! tbl row)
     (dotimes [col-idx col-num]
-      (dommy/append! row (node [:td {:class (str row-idx col-idx)}] )))))
+      (dommy/append! row (node [:td {:class (str row-idx col-idx)}])))))
 
 (defn- create-board [size]
     (dotimes [r size]
@@ -41,20 +41,10 @@
     (create-board 4)
     (update! game-state)))
 
-;; Event handling
-(defn step-state
-  [evt]
-  ; left = 37
-  ; up = 38
-  ; right = 39
-  ; down = 40
-  (when (= 37 (.-keyCode evt))
-    (update! (calc-state! :left game-state))))
-
-(dommy/listen! (sel1 :body) :keyup step-state!)
+(update! game-state)
 
 
-;; Update
+;; Caculate next state
 (defn get-col [col-idx state]
   (apply hash-map
          (flatten
@@ -67,40 +57,11 @@
 (defn filter-zeros [row]
   (map #(second %1) (filter #(not= 0 (second %)) row)))
 
-
-(defn m2 [prev tail acc]
-  (cond
-   (= (count tail) 0)
-   (reverse (if (= prev (first tail))
-              (cons (+ prev (first tail)) acc)
-              (cons prev acc)))
-   (= (count tail) 1)
-   (reverse (if (= prev (first tail))
-              (cons (+ prev (first tail)) acc)
-              (cons (first tail) (cons prev acc))))
-   :else (recur (first (rest tail))
-             (rest (rest tail))
-             (if (= prev (first tail))
-               (cons (+ prev (first tail)) acc)
-               (cons (first tail) (cons prev acc))))))
-
-
-(loop [prev 1 tail [1 2] acc []]
-  (cond
-   (<= (count tail) 1)
-   (reverse
-    (if (= prev (first tail))
-      (cons (+ prev (first tail)) acc)
-      (if (nil? (first tail))
-        (cons prev acc)
-        (cons (first tail) (cons prev acc)))))
-   :else (recur (first (rest tail))
-             (rest (rest tail))
-             (if (= prev (first tail))
-               (cons (+ prev (first tail)) acc)
-               (cons (first tail) (cons prev acc))))))
-
-(m2 3 [1 2 2 2 2 5 5 5] [])
+(def game-state2
+  {0 {0 2, 1 3, 2 0, 3 3},
+   1 {0 2, 1 0, 2 0, 3 0},
+   2 {0 2, 1 0, 2 0, 3 0},
+   3 {0 2, 1 0, 2 0, 3 0}})
 
 (defn merge-sum [[start & coll]]
   (flatten (reverse
@@ -109,23 +70,41 @@
                 (cons (list (+ %2 (first %1))) (rest %1))
                 (cons %2 %1)) (list start) coll))))
 
-
-(def game-state2
-  {0 {0 2, 1 3, 2 0, 3 3},
-   1 {0 2, 1 0, 2 0, 3 0},
-   2 {0 2, 1 0, 2 0, 3 0},
-   3 {0 2, 1 0, 2 0, 3 0}})
-
-
-(zero-pad 4 (merge-sum (reverse (filter-zeros (get game-state2 0)))))
-
 (defn zero-pad [len coll]
   (concat coll (take (- len (count coll)) (repeat 0))))
 
+(defn calc-row [row]
+  (apply hash-map
+         (flatten (map-indexed vector
+                               (zero-pad (count row) (merge-sum
+                                            (filter-zeros row)))))))
+
+(defn calc-left-merge [state]
+  (map #(calc-row (second %)) (map identity state)))
+
+(defn calc-state! [dir state]
+  (let [updated-state
+        (cond
+         (= dir :left)
+         (apply hash-map
+                (flatten(map-indexed vector (calc-left-merge state)))))]
+    (do
+      (def game-state updated-state)
+      updated-state)))
+
+;; (calc-state! :left game-state2)
 
 
+;; Event handling
+(defn step-state!
+  [evt]
+  ; left = 37
+  ; up = 38
+  ; right = 39
+  ; down = 40
+  (when (= 37 (.-keyCode evt))
+    (update! (calc-state! :left game-state))))
 
+;;(calc-state! :left game-state)
 
-
-
-
+(dommy/listen! (sel1 :body) :keyup step-state!)
